b679e17b95e9b3059102075aa4d3cf2945fda020
ohlavrac <joaovicrcarvalho@gmail.com>
2024-08-27 07:19:00 -0300

    init project

---
+++ src/main/java/com/ohlavrac/cpdmanager/CpdmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ohlavrac.cpdmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpdmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpdmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohlavrac/cpdmanager/CpdmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohlavrac.cpdmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpdmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
