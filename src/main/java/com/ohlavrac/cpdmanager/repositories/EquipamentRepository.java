5090485e2780e158575f90342b6331e7ad6300a0
ohlavrac <joaovicrcarvalho@gmail.com>
2024-08-30 10:16:40 -0300

    feat: add custom query to find equipaments by type

--- src/main/java/com/ohlavrac/cpdmanager/repositories/EquipamentRepository.java
+++ src/main/java/com/ohlavrac/cpdmanager/repositories/EquipamentRepository.java
@@ -1,13 +1,18 @@
 package com.ohlavrac.cpdmanager.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ohlavrac.cpdmanager.domain.entities.equipaments.EquipamentsEntity;
+import com.ohlavrac.cpdmanager.domain.enums.EquipamentTypeEnum;
 
 @Repository
 public interface EquipamentRepository extends JpaRepository<EquipamentsEntity, UUID> {
 
+    @Query("SELECT e FROM EquipamentsEntity e WHERE e.equipamenttype=?1")
+    List<EquipamentsEntity> findEquipamentByType(EquipamentTypeEnum equipamenttype);
 }
