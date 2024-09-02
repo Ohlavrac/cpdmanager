363d9a83806697ebecaa6b812cf6f68e6068c3b8
ohlavrac <joaovicrcarvalho@gmail.com>
2024-09-02 07:26:21 -0300

    feat: add Exception for equipament not found

---
+++ src/main/java/com/ohlavrac/cpdmanager/exceptions/EquipamentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ohlavrac.cpdmanager.exceptions;
+
+public class EquipamentNotFoundException extends RuntimeException {
+    public EquipamentNotFoundException(String message) {
+        super(message);
+    }
+}
